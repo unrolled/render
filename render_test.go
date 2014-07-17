7a4e96655a4ae99e3b18c2e74bb5bac876d51596
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-07-17 23:30:17 +0000

    added simple tests

--- render_test.go
+++ render_test.go
@@ -2,11 +2,13 @@ package render
 
 import (
 	"encoding/xml"
+	"github.com/eknkc/amber"
 	"html/template"
 	"math"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -419,7 +421,7 @@ func TestRenderDefaultCharsetHTML(t *testing.T) {
 
 	expect(t, res.Code, 200)
 	expect(t, res.Header().Get(ContentType), ContentHTML+"; charset=UTF-8")
-	//ContentLength should be deferred to the ResponseWriter and not Render
+	// ContentLength should be deferred to the ResponseWriter and not Render
 	expect(t, res.Header().Get(ContentLength), "")
 	expect(t, res.Body.String(), "<h1>Hello gophers</h1>\n")
 }
@@ -464,7 +466,7 @@ func TestRenderNoRace(t *testing.T) {
 
 		expect(t, res.Code, 200)
 		expect(t, res.Header().Get(ContentType), ContentHTML+"; charset=UTF-8")
-		//ContentLength should be deferred to the ResponseWriter and not Render
+		// ContentLength should be deferred to the ResponseWriter and not Render
 		expect(t, res.Header().Get(ContentLength), "")
 		expect(t, res.Body.String(), "<h1>Hello gophers</h1>\n")
 		done <- true
@@ -474,6 +476,60 @@ func TestRenderNoRace(t *testing.T) {
 	go doreq()
 	<-done
 	<-done
+}
+
+type Amber struct {
+	Head
+	Template *template.Template
+}
+
+func (a Amber) Render(w http.ResponseWriter, v interface{}) error {
+	a.Head.Write(w)
+	return a.Template.Execute(w, v)
+}
+
+func TestRenderAmberTemplate(t *testing.T) {
+	dir := "fixtures/amber/"
+	render := New(Options{})
+
+	templates, err := amber.CompileDir(dir, amber.DefaultDirOptions, amber.DefaultOptions)
+	if err != nil {
+		t.Errorf("Could not compile Amber templates at " + dir)
+	}
+
+	a := Amber{
+		Head: Head{
+			ContentType: ContentHTML,
+			Status:      http.StatusOK,
+		},
+		Template: templates["example"],
+	}
+
+	v := struct {
+		VarOne string
+		VarTwo string
+	}{
+		"Contact",
+		"Content!",
+	}
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		render.Render(w, a, v)
+	})
+
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/foo", nil)
+	h.ServeHTTP(res, req)
+
+	body := res.Body.String()
+
+	checkCompile := strings.Index(body, `<div id="header">`) != -1
+	checkVarOne := strings.Index(body, `<li>Contact</li>`) != -1
+	checkVarTwo := strings.Index(body, `<li>Content!</li>`) != -1
+
+	expect(t, checkCompile && checkVarOne && checkVarTwo, true)
+	expect(t, res.Code, 200)
+	expect(t, res.Header().Get(ContentType), ContentHTML)
 }
 
 /* Test Helpers */
